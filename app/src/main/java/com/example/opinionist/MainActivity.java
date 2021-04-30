bd315b9732fe38a0cbd40510b0f9340ed5ae90bb
Jousua Giurculete <jousua.giurculete@wsu.edu>
2021-04-29 22:22:45 -0700

    Cleaned up main menu page(will be login page). Added logout button

--- app/src/main/java/com/example/opinionist/MainActivity.java
+++ app/src/main/java/com/example/opinionist/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.opinionist;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,143 +19,50 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    EditText subComment, retComment;
-    DatabaseReference reff;
-    Comment newComment;
-    Button btnSubmit, btnretrieve, btndelete;
-    TextView viewComment;
-    long maxid = 0;
+    Button btnRegister, btnComments, btnLogout;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* SEND COMMENT TO SERVER */
-        // check connection to firebase server
-        Toast.makeText(MainActivity.this, "Firebase connection success!", Toast.LENGTH_LONG).show();
+        btnRegister = findViewById(R.id.buttonMainRegister);
+        btnComments = findViewById(R.id.buttonMainComments);
+        btnLogout = findViewById(R.id.buttonMainLogout);
+        mAuth = FirebaseAuth.getInstance();
 
-        // get comment from comment textbox
-        subComment = (EditText)findViewById(R.id.editComment);
-        newComment = new Comment();
+        if(mAuth.getCurrentUser() != null) {
+            btnLogout.setVisibility(View.VISIBLE);
+        }
 
-        // create new reference to database. all comment instances will fall under 'comments'
-        // must be done for submission and retrieval
-        reff = FirebaseDatabase.getInstance().getReference("comments");
-
-        // get number of comments in database and set it to maxid
-        // so that next comment can be maxid+1
-        reff.addValueEventListener(new ValueEventListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()) {
-                    maxid=snapshot.getChildrenCount();
+            public void onClick(View v) {
+                startActivity( new Intent(getApplicationContext(), Register.class) );
+            }
+        });
+
+        btnComments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(getApplicationContext(), Comments.class) );
+            }
+        });
+
+        // Sign out user
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if( mAuth.getCurrentUser() != null ) {
+                    mAuth.signOut();
+                    btnLogout.setVisibility(View.INVISIBLE);
                 }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
-
-        // send comment to firebase server when button is clicked
-        btnSubmit = (Button)findViewById(R.id.button);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                newComment.setComment(String.valueOf(subComment.getText()));
-                newComment.setLikes(0);
-
-                reff.child(String.valueOf(maxid+1)).setValue(newComment);
-                Toast.makeText(MainActivity.this, "Comment inserted successfuly!", Toast.LENGTH_LONG).show();
-            }
-        });
-
-
-
-        /* RETRIEVE DATA */
-        // get xml items
-        retComment = (EditText)findViewById(R.id.editRetrieveComment);
-        btnretrieve = (Button)findViewById(R.id.button2);
-        btndelete = (Button)findViewById(R.id.button3);
-        viewComment = (TextView)findViewById(R.id.textViewComment);
-
-        // check when retrieve button is clicked
-        btnretrieve.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // grab a snapshot of comments array in database
-                reff.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if( snapshot.child(String.valueOf(retComment.getText())).exists() ) { // comment ID exists
-                            viewComment.setText(snapshot.child(String.valueOf(retComment.getText())).getValue().toString());
-                        } else { // comment ID does not exists
-                            viewComment.setText("Comment ID does not exist");
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-
-            }
-        });
-
-
-        /* DELETE AN ITEM */
-        /*  ISSUE:  comment ID can get confusing when deleting a comment from DB. For example,
-                    if you delete comment ID 1 but comment ID 2 exists, we need to find a way
-                    to refill ID 1. Maybe create an array that stores every ID that is empty and check that array when
-                    inserting a new comment before incrementing maxID
-         */
-        // check when retrieve button is clicked
-        btndelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // grab a snapshot of comments array in database
-                reff.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if( snapshot.child(String.valueOf(retComment.getText())).exists() ) { // comment ID exists
-                            snapshot.child(String.valueOf(retComment.getText())).getRef().removeValue();
-                            viewComment.setText("Comment removed");
-                        } else { // comment ID does not exists
-                            viewComment.setText("Comment ID does not exist");
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-
-            }
-        });
-
-
-
 
 
     }
-
-//    public void doInc(View v)
-//    {
-//        TextView msg=(TextView) findViewById(R.id.textView);
-//        msg.setText("jfdshfjfhkaflhhfajlfhakfh");
-//
-//        NetThread nt=new NetThread();
-//        nt.saveTV(msg);
-//        nt.start();
-//
-//    }
 }
 
 
